a044be652a7dbae8376163937614ade35f3c0d04
mtellesy <m.al-tellesy@uot.edu.ly>
2016-12-30 16:12:52 +0200

    x

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -2099,6 +2099,24 @@ namespace UOTCS_android
 			// aapt resource value: 0x7f060093
 			public const int FragmentContainer = 2131099795;
 			
+			// aapt resource value: 0x7f0600aa
+			public const int MO1 = 2131099818;
+			
+			// aapt resource value: 0x7f0600ab
+			public const int MO2 = 2131099819;
+			
+			// aapt resource value: 0x7f0600ac
+			public const int MO3 = 2131099820;
+			
+			// aapt resource value: 0x7f0600ad
+			public const int MO4 = 2131099821;
+			
+			// aapt resource value: 0x7f0600ae
+			public const int MO5 = 2131099822;
+			
+			// aapt resource value: 0x7f0600af
+			public const int MO6 = 2131099823;
+			
 			// aapt resource value: 0x7f06009c
 			public const int SA1 = 2131099804;
 			
@@ -2114,8 +2132,83 @@ namespace UOTCS_android
 			// aapt resource value: 0x7f0600a0
 			public const int SA5 = 2131099808;
 			
+			// aapt resource value: 0x7f0600a1
+			public const int SA6 = 2131099809;
+			
+			// aapt resource value: 0x7f0600a3
+			public const int SU1 = 2131099811;
+			
+			// aapt resource value: 0x7f0600a4
+			public const int SU2 = 2131099812;
+			
+			// aapt resource value: 0x7f0600a5
+			public const int SU3 = 2131099813;
+			
+			// aapt resource value: 0x7f0600a6
+			public const int SU4 = 2131099814;
+			
+			// aapt resource value: 0x7f0600a7
+			public const int SU5 = 2131099815;
+			
+			// aapt resource value: 0x7f0600a8
+			public const int SU6 = 2131099816;
+			
 			// aapt resource value: 0x7f060095
 			public const int ScheduleFrame = 2131099797;
+			
+			// aapt resource value: 0x7f0600bf
+			public const int TH1 = 2131099839;
+			
+			// aapt resource value: 0x7f0600c0
+			public const int TH2 = 2131099840;
+			
+			// aapt resource value: 0x7f0600c1
+			public const int TH3 = 2131099841;
+			
+			// aapt resource value: 0x7f0600c2
+			public const int TH4 = 2131099842;
+			
+			// aapt resource value: 0x7f0600c3
+			public const int TH5 = 2131099843;
+			
+			// aapt resource value: 0x7f0600c4
+			public const int TH6 = 2131099844;
+			
+			// aapt resource value: 0x7f0600b1
+			public const int TU1 = 2131099825;
+			
+			// aapt resource value: 0x7f0600b2
+			public const int TU2 = 2131099826;
+			
+			// aapt resource value: 0x7f0600b3
+			public const int TU3 = 2131099827;
+			
+			// aapt resource value: 0x7f0600b4
+			public const int TU4 = 2131099828;
+			
+			// aapt resource value: 0x7f0600b5
+			public const int TU5 = 2131099829;
+			
+			// aapt resource value: 0x7f0600b6
+			public const int TU6 = 2131099830;
+			
+			// aapt resource value: 0x7f0600b8
+			public const int WE1 = 2131099832;
+			
+			// aapt resource value: 0x7f0600b9
+			public const int WE2 = 2131099833;
+			
+			// aapt resource value: 0x7f0600ba
+			public const int WE3 = 2131099834;
+			
+			// aapt resource value: 0x7f0600bb
+			public const int WE4 = 2131099835;
+			
+			// aapt resource value: 0x7f0600bc
+			public const int WE5 = 2131099836;
+			
+			// aapt resource value: 0x7f0600bd
+			public const int WE6 = 2131099837;
 			
 			// aapt resource value: 0x7f060086
 			public const int action0 = 2131099782;
@@ -2363,20 +2456,20 @@ namespace UOTCS_android
 			// aapt resource value: 0x7f060016
 			public const int multiply = 2131099670;
 			
-			// aapt resource value: 0x7f0600a2
-			public const int nav_announcements = 2131099810;
+			// aapt resource value: 0x7f0600c5
+			public const int nav_announcements = 2131099845;
 			
-			// aapt resource value: 0x7f0600a3
-			public const int nav_messages = 2131099811;
+			// aapt resource value: 0x7f0600c6
+			public const int nav_messages = 2131099846;
 			
-			// aapt resource value: 0x7f0600a4
-			public const int nav_myCourses = 2131099812;
+			// aapt resource value: 0x7f0600c7
+			public const int nav_myCourses = 2131099847;
 			
-			// aapt resource value: 0x7f0600a5
-			public const int nav_schedule = 2131099813;
+			// aapt resource value: 0x7f0600c8
+			public const int nav_schedule = 2131099848;
 			
-			// aapt resource value: 0x7f0600a6
-			public const int nav_timetable = 2131099814;
+			// aapt resource value: 0x7f0600c9
+			public const int nav_timetable = 2131099849;
 			
 			// aapt resource value: 0x7f06006e
 			public const int nav_view = 2131099758;
@@ -2522,11 +2615,23 @@ namespace UOTCS_android
 			// aapt resource value: 0x7f06009a
 			public const int tableRow1 = 2131099802;
 			
-			// aapt resource value: 0x7f0600a1
-			public const int tableRowMON = 2131099809;
+			// aapt resource value: 0x7f0600a9
+			public const int tableRowMON = 2131099817;
 			
 			// aapt resource value: 0x7f06009b
 			public const int tableRowSAT = 2131099803;
+			
+			// aapt resource value: 0x7f0600a2
+			public const int tableRowSUN = 2131099810;
+			
+			// aapt resource value: 0x7f0600be
+			public const int tableRowTHU = 2131099838;
+			
+			// aapt resource value: 0x7f0600b0
+			public const int tableRowTUE = 2131099824;
+			
+			// aapt resource value: 0x7f0600b7
+			public const int tableRowWED = 2131099831;
 			
 			// aapt resource value: 0x7f060090
 			public const int text = 2131099792;
