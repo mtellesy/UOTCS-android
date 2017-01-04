08d1ccb211eaf95582d826fa2a2f037f8a5f60b8
amiraattia94 <amira.j.attia@gmail.com>
2017-01-04 13:39:32 +0200

    :(

--- Fragments/UserInformationFragment.cs
+++ Fragments/UserInformationFragment.cs
@@ -10,25 +10,39 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Android.Support.V4.Widget;
+using SupportFragment = Android.Support.V4.App.Fragment;
+using SupportFragmentManager = Android.Support.V4.App.FragmentManager;
+using Android.Support.V4.App;
 
 namespace UOTCS_android.Fragments
 {
     public class UserInformationFragment : Android.Support.V4.App.Fragment
     {
+        private Username username;
+        private UserInformationFragment userInformation;
+        private UserMoreInfomationFragment userMoreInformation;
+        private FragmentActivity myContext;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your fragment here
         }
-
+        public override void OnAttach(Activity activity)
+        {
+            base.OnAttach(activity);
+            myContext = (FragmentActivity)activity;
+        }
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
             //
             View view = inflater.Inflate(Resource.Layout.UserInfomationsF, container, false);
-
+        
             return view;
         }
+        
     }
 }
