3b571213e34230ee48f240b1f6c497e776eb8c2c
amiraattia94 <amira.j.attia@gmail.com>
2017-02-18 22:05:37 +0200

    action bar fixed

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -3649,46 +3649,43 @@ namespace UOTCS_android
 			public const int select_dialog_singlechoice_material = 2130903119;
 			
 			// aapt resource value: 0x7f030050
-			public const int SendAnnouncement = 2130903120;
+			public const int sendMessage = 2130903120;
 			
 			// aapt resource value: 0x7f030051
-			public const int sendMessage = 2130903121;
+			public const int SendMessageAnnouncementF = 2130903121;
 			
 			// aapt resource value: 0x7f030052
-			public const int SendMessageAnnouncementF = 2130903122;
+			public const int sendToolBar = 2130903122;
 			
 			// aapt resource value: 0x7f030053
-			public const int sendToolBar = 2130903123;
+			public const int SentMessagesF = 2130903123;
 			
 			// aapt resource value: 0x7f030054
-			public const int SentMessagesF = 2130903124;
+			public const int Settings = 2130903124;
 			
 			// aapt resource value: 0x7f030055
-			public const int Settings = 2130903125;
+			public const int support_simple_spinner_dropdown_item = 2130903125;
 			
 			// aapt resource value: 0x7f030056
-			public const int support_simple_spinner_dropdown_item = 2130903126;
+			public const int Timetable = 2130903126;
 			
 			// aapt resource value: 0x7f030057
-			public const int Timetable = 2130903127;
+			public const int TimetableAndMidExamF = 2130903127;
 			
 			// aapt resource value: 0x7f030058
-			public const int TimetableAndMidExamF = 2130903128;
+			public const int transcript = 2130903128;
 			
 			// aapt resource value: 0x7f030059
-			public const int transcript = 2130903129;
+			public const int transcriptF = 2130903129;
 			
 			// aapt resource value: 0x7f03005a
-			public const int transcriptF = 2130903130;
+			public const int UserInfomationsF = 2130903130;
 			
 			// aapt resource value: 0x7f03005b
-			public const int UserInfomationsF = 2130903131;
+			public const int UserMoreInformationF = 2130903131;
 			
 			// aapt resource value: 0x7f03005c
-			public const int UserMoreInformationF = 2130903132;
-			
-			// aapt resource value: 0x7f03005d
-			public const int usernameF = 2130903133;
+			public const int usernameF = 2130903132;
 			
 			static Layout()
 			{
